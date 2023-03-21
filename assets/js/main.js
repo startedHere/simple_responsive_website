6d1549bd1243b7c15230abbd81cbdbcdce7c0eb1
startedHere <ugwu.chukwuma@outlook.com>
2023-03-21 15:07:09 +0100

    Added Active Section Nav-Link Indicator to Webpage

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,41 @@ function endOfVideo() {
 
 // When Video ends...
 videoFile.addEventListener('ended', endOfVideo);
+
+//! display Scroll-Up Button beside The Scroll Bar While Scrolling...
+///////////////////////////////////////
+function scrollUp() {
+	const scrollUp = document.getElementById('scroll-up');
+
+	//# When the Scroll is Higher than 200vp height , add the 'show-scroll' class to the tag with the scroll-up class
+	if (this.scrollY >= 400) {
+		scrollUp.classList.add('show-scroll');
+	} else {
+		scrollUp.classList.remove('show-scroll');
+	}
+}
+window.addEventListener('scroll', scrollUp);
+
+//! Scrolling A Section will Activate Corresponding Section Link on The Nav Bar...
+///////////////////////////////////////////////////////////////////////////
+function scrollActive() {
+	const scrollY = window.pageYOffset;
+	const sections = document.querySelectorAll('section[id]');
+
+	sections.forEach(current => {
+		const sectionTop = current.offsetTop - 50;
+		const sectionHeight = current.offsetHeight;
+		const sectionId = current.getAttribute('id');
+		// const sectionId = current.id;
+
+		// _ If Statement to define Border Boundaries for An Active Section on a Webpage...
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.add('active-link');
+		} else {
+			document
+				.querySelector(`.nav__menu a[href*=${sectionId}]`)
+				.classList.remove('active-link');
+		}
+	});
+}
+window.addEventListener('scroll', scrollActive);
